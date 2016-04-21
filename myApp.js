3488493cb45b655ebc48da71691b99189a5c9d63
crtnywllr <courtneycmeadows@gmail.com>
2016-04-21 13:22:30 -0600

    input and story separated, validation working

--- myApp.js
+++ myApp.js
@@ -1,4 +1,14 @@
-angular.module('myApp', [])
+angular.module('myApp', ['ngMessages'])
     .controller("myCtrl", function ($scope) {
         $scope.data = {};
+
+        $scope.gender = {};
+
+        $scope.inputHide = false;
+
+        $scope.submit = function () {
+            console.log($scope.data);
+            console.log($scope.madInput);
+            $scope.inputHide = true;
+        }
     });
